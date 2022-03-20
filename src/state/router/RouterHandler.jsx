5d852cc04496e3ad32d1ea792b6af83c78b3f6e7
etsydannegm <danielgarcia@etsy.com>
2022-03-19 21:30:30 -0600

    feat: React? WTF

--- src/state/router/RouterHandler.jsx
+++ src/state/router/RouterHandler.jsx
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Loading from '@components/Loading';
 
